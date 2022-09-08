fe25fd6a66ef9653c26de2f2ae75c0677a1c01f0
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-09-08 18:01:53 -0400

    Changes

--- ClassExercises/Class02/lists.py
+++ ClassExercises/Class02/lists.py
@@ -7,79 +7,79 @@ print(colors)
 ##################################
 # Accessing an element in a list #
 ##################################
-# print(colors[0])
-# print(colors[1])
+print(colors[0])
+print(colors[2])
 
 ##############################
 # Accessing a part of a list #
 ##############################
-# indices=['zero','one','two','three','four','five']
-# print(indices[2:4])
-# print(indices[2:]
-# print(indices[:4])
-# print(:)
+indices=['zero','one','two','three','four','five']
+print(indices[2:4])
+print(indices[2:])
+print(indices[:4])
 
 ####################
 # Negative indices #
 ####################
-# print(indices[-1])
-# print(indices[-2:])
+print(indices[-1])
+print(indices[-2:])
 
 ######################
 # Reassigning values #
 ######################
-# colors=['caramel','gold','silver','occur']
-# print(colors)
-# colors[1]="aureum"
-# print(colors)
-# colors[1:2]=["gold","argentine"]
+colors=['caramel','gold','silver','occur']
+print(colors)
+colors[1]="aureum"
+print(colors)
+colors[1:2]=["gold","argentine"]
+print(colors)
 
 #######################
 # Iterating over list #
 #######################
-# for i in [1,2,3]:
-#         if i%2==0:
-#                print("{i} is even".format(i))
-#         else:
-#                print("{i} is odd".format(i))
+for i in [100,235,35]:
+        if i%2==0:
+               print("{} is even".format(i))
+        else:
+               print("{} is odd".format(i))
 
-# numbers=[8,25,32, 45, 29, 38, 66]
-# for i in numbers:
-#         if i%2==0:
-#                print("{i} is even".format(i))
-#         else:
-#                print("{i} is odd".format(i))
+numbers=[8,25,32, 45, 29, 38, 66]
+for i in numbers:
+        if i%2==0:
+               print("{} is even".format(i))
+        else:
+               print("{} is odd".format(i))
 
-# numberList= range(0,20)
-# print(numberList)
-# for i in numberList:
-#        if i%2==0:
-#               print("{i} is even".format(i))
-#        else:
-#               print("{i} is odd".format(i))
+numberList= range(0,20)
+print(numberList)
+for i in numberList:
+       if i%2==0:
+              print("{} is even".format(i))
+       else:
+              print("{} is odd".format(i))
 
 ###########################
 # Add elements at the end #
 ###########################
-# exampleList=["boat","home","family"]
-# print(exampleList)
-# exampleList.append("pencil")
-# exampleList.append("watch")
-# print(exampleList)
+exampleList=["boat","home","family"]
+print(exampleList)
+exampleList.append("pencil")
+exampleList.append("watch")
+print(exampleList)
 
-# growingList =[]
-# print(growingList)
-# growingList.append(1)
-# growingList.append(25)
-# growingList.append(90)
-# print(growingList)
+growingList =[]
+print(growingList)
+growingList.append(1)
+growingList.append(25)
+growingList.append(90)
+print(growingList)
 
 ###############
 # List Length #
 ###############
-# exampleList = ["student1","student2","student3","student4"]
-# print(len(exampleList))
-# print(len(exampleList[0]))
+exampleList = ["student1","student2","student3","student4"]
+print(len(exampleList))
+print(len(exampleList[0]))
 
 
 ##########################
